a596d4675bc9cf39b0ffe91234c7ef758260fced
MirekKopec <mkopec95@gmail.com>
2017-11-17 00:00:53 +0100

    oop task 10.4

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// scripts.js
+
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+
+	var $phoneList = $('<ul class="phone-list"></ul>');
+
+Phone.prototype.printInfo = function() {
+
+	var listElement = "The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".";
+
+	$phoneList.append('<li>'+ listElement +'</li>');
+
+	$('h1').after($phoneList);
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var GalaxyS8 = new Phone("Samsung", 2000, "black");
+var MotoZ = new Phone("Motorola", 1500, "red");
+
+var pageHeader = document.getElementsByTagName('h1');
+	pageHeader[0].innerHTML ="Phone's list: "
+
+iPhone6S.printInfo();
+MotoZ.printInfo();
+GalaxyS8.printInfo();
+
+var liStyle = $("li");
+    liStyle.each(function(index, element) {
+
+      $("li").css({"marign-left": "100px", "text-align": "left"});
+});
