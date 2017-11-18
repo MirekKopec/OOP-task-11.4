5f8bf7ce6a5ff8caef5fb53a54b71fa68c0d60c1
MirekKopec <mkopec95@gmail.com>
2017-11-18 12:27:18 +0100

    added task 11.4

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,24 @@ var liStyle = $("li");
 
       $("li").css({"marign-left": "100px", "text-align": "left"});
 });
+
+
+function Button(text) {
+	this.text = text || 'Hello';
+}
+
+Button.prototype = {
+	create: function () {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		this.$element.appendTo($('body'));
+	}
+}
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
